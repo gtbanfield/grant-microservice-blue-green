bfe408110d6b9776309e2e6ef63051b9c91cd905
Grant Banfield <gtbanfield@gmail.com>
2025-02-27 21:25:18 +0000

    test

---
+++ main/java/com/example/actuatordemo/config/ActuatorSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.actuatordemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
+
+@Configuration
+public class ActuatorSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    http.headers(headers ->
+            headers
+            	   .httpStrictTransportSecurity(hstsConfig -> {
+            	   	hstsConfig.includeSubDomains(true);
+            	   	hstsConfig.maxAgeInSeconds(350000);
+		   })
+		   .xssProtection(
+			  xss -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK)
+		   ).contentSecurityPolicy(
+			  cps -> cps.policyDirectives("script-src 'self'")
+		   ));
+    return http.csrf(AbstractHttpConfigurer::disable).build();
+  }
+}

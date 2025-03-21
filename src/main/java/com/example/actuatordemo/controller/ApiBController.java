49ae7d2cac3c29b356a3bcb2b35cea290cd4d42a
Grant Banfield <gtbanfield@gmail.com>
2025-03-21 07:02:02 +0000

    test

---
+++ src/main/java/com/example/actuatordemo/controller/ApiBController.java
@@ -0,0 +1,13 @@
+package com.example.actuatordemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiBController {
+
+    @GetMapping("/api/b/data")
+    public String provideData() {
+        return "Data from API B";
+    }
+}
